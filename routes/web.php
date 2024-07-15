07ae9c8b3ffb02337d434d5f7717e09bac75d368
Jamily Tavares <jamily751@hotmail.com>
2024-07-15 10:32:12 -0300

    star controller

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-Route::get('/', function(){
-    return view('welcome');
-});
-
-Route::any('/any', function(){
-    return "permite todo tipo de acesso hppt (put, delete, delete, post)";
-});
-
-Route::match(['put', 'delete'], '/match', function(){
-    return "permite apenas acessos definidos";
-});
-
-Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
-    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
-});
-
-Route::get('/produt/{id}/{cat}', function($id, $cat){
-    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
-});
-
-Route::redirect('/sobre', '/empresa');
-Route::view('/empresa', 'site/empresa');
-
-Route::get('/news', function(){
-    return view('news');
-})->name('noticias');
-
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
-});
+Route::get('/', [ProdutoController::class, 'index'] );
