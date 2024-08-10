49c80a2a7f439f11d1089cce69c3e10c39f9e4e1
Jamily <jamily751@hotmail.com>
2024-08-10 19:24:01 -0300

    created migrations

--- database/migrations/2024_07_30_125438_produtos.php
+++ database/migrations/2024_07_30_125438_produtos.php
@@ -6,28 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
-            $table->text('descricao');
-            $table->double('preco', 10, 2);
-            $table->string('slug');
-            $table->string('imagem');
-            $table->unsignedInteger();
-            $table->timestamps();
-        });
-    }
+    
+public function up(): void
+{
+    Schema::create('produtos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome');
+        $table->text('descricao');
+        $table->double('preco', 10, 2);
+        $table->string('slug');
+        $table->string('imagem');
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('produtos');
-    }
+        $table->unsignedBigInteger('id_user');
+        $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+        $table->unsignedBigInteger('id_categoria');
+        $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('cascade')->onUpdate('cascade');
+        
+        $table->timestamps();
+    });
+}
+
+public function down(): void
+{
+    Schema::dropIfExists('produtos');
+}
 };
