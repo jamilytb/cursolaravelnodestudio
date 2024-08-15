c16b073abdc445797aef1a978b48851cc4564bdc
Jamily <jamily751@hotmail.com>
2024-08-14 23:10:02 -0300

    creating faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Categoria;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,8 +12,6 @@ class DatabaseSeeder extends Seeder
     
     public function run(): void
     {
-      $this->call([
-        UsersSeeder::class,
-      ]);
+      $this->call(CategoriasSeeder::class);
     }
 }
