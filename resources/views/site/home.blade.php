db17ff7945198ddcd19baed5f0db48ee1c464940
Jamily Tavares <jamily751@hotmail.com>
2024-09-09 10:46:27 -0300

    created includes

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,6 +2,12 @@
 @section('title', 'Essa é a página HOME')
 @section('conteudo')
 
+@include('includes.mensagem', ['titulo' => 'Mensagem de sucesso!'])
 
+@component('components.sidebar')
+    @slot('paragrafo')
+        Texto qualquer vindo do slot  
+    @endslot
+@endcomponent    
 
 @endsection
